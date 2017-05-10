acfe2e7ee6429a45e410504779219645ae6b51f3
David <alienyidavid4christ@gmail.com>
2017-05-09 23:26:59 -0700

    istall dependecy and set up the config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'kOGW4guYfVcG7NHhO5kD5Y0w5',  
+  consumer_secret: '1MZf6Vsn4pdXPTCeVM9i8oo0jytAKJ32BXjcjxINEVO7ctQwY3',
+  access_token: '841670113602461701-yUDXCdNtiGf9dHBMSGgYJl8BCn8lWwJ',  
+  access_token_secret: 'SrTMTwCNw0rVmC31c3yaHJUuIJymOz1Hl5ZleVpgcveea'
+}
